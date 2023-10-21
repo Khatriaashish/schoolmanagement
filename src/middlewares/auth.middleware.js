4ae9cf771d9ceb78416f5197dab84b98acbc70c7
Ashish Khatri <60392989+Khatriaashish@users.noreply.github.com>
2023-10-21 15:25:18 +0545

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -32,9 +32,6 @@ const checkLogin = (req, res, next)=>{
                     "name": "Ashish Khatri",
                     "email": "khatriaashish1@gmail.com",
                     "role": "teacher",
-                    "status": "active",
-                    "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOnsibmFtZSI6IkFzaGlzaCBLaGF0cmkiLCJlbWFpbCI6ImtoYXRyaWFhc2hpc2gxQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsInN0YXR1cyI6ImFjdGl2ZSIsInRva2VuIjoibnVsbCIsInBhc3N3b3JkIjoiJDJhJDEwJEtVQzRzWjVqYjh1dHlkZFFTUms3bHVtUFBCcy9pcEFDbDZJSVZIejNSUUJLNkVsTHNTMEJDIn0sImlhdCI6MTY5Nzg3MzQ0MywiZXhwIjoxNjk3ODc3MDQzfQ.ZUKPh7h6yLMephA_CNp00Li0Yv9jHhEBlznUiAxFSho",
-                    "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOnsibmFtZSI6IkFzaGlzaCBLaGF0cmkiLCJlbWFpbCI6ImtoYXRyaWFhc2hpc2gxQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsInN0YXR1cyI6ImFjdGl2ZSIsInRva2VuIjoibnVsbCIsInBhc3N3b3JkIjoiJDJhJDEwJEtVQzRzWjVqYjh1dHlkZFFTUms3bHVtUFBCcy9pcEFDbDZJSVZIejNSUUJLNkVsTHNTMEJDIn0sImlhdCI6MTY5Nzg3MzQ0MywiZXhwIjoxNjk3OTU5ODQzfQ.5vRvdeusZx2R5naXhi9wBlUv5isHJJSaNWnI1NHVtgQ",
                     "password": "$2a$10$KUC4sZ5jb8utyddQSRk7lumPPBs/ipACl6IIVHz3RQBK6ElLsS0BC"
                 }
                 if(userDetail){
